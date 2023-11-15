65fbde7e446186b9583e5d850c80a8124b0c18c4
葉明淳 <148878625+Emyeee@users.noreply.github.com>
2023-11-15 20:40:16 +0800

    Update 2-2-3.sql

--- 2-2-3.sql
+++ 2-2-3.sql
@@ -6,4 +6,7 @@ FROM `physionet-data.mimiciv_hosp.admissions` as a
 inner join `physionet-data.mimiciv_derived.kdigo_stages` as k
 on k.subject_id = a.subject_id
 -- 路徑: physionet-data.mimiciv_hosp.admissions -> 欄位: hospital_expire_flag (die in hospital)
-where a.hospital_expire_flag = 0 
+inner join `physionet-data.mimiciv_derived.icustay_detail` as i
+on i.subject_id = a.subject_id
+
+where a.hospital_expire_flag = 1 and (datetime_diff(a.dischtime, i.icu_outtime, hour) < 1)
